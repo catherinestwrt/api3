c0b83882cbe9451ee5f63274cbbdfbd3bec175d0
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-04 10:49:55 -0700

    suppliersRouter returns given name

--- api/server.js
+++ api/server.js
@@ -1,11 +1,19 @@
 console.log('server.js')
 
 const express = require('express');
-
 const server = express();
 
-server.get('/', (req, res) => {
+const suppliersRouter = require('../suppliers/suppliersRouter');
 
+// global middleware
+server.use(express.json());
+
+//route
+server.use('/suppliers', suppliersRouter);
+
+
+server.get('/', (req, res) => {
+  res.status(200).send({hello: "world"});
 });
 
 module.exports = server;
